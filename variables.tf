70733ad86dcb92e87311b2ae3a1c56419a346dea
feliperfmarques <feliperfmarques@gmail.com>
2021-01-04 01:51:45 -0300

    Removing unnecessary module variables.

--- variables.tf
+++ variables.tf
@@ -2,19 +2,9 @@
 # Account variables
 ##############################################################################
 
-variable ibmcloud_api_key {
-    description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
-    type        = string
-}
-
 variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed"
     type        = string
-}
-
-variable generation {
-    description = "Generation of VPC where cluster is deployed"
-    default     = 2
 }
 
 variable resource_group {
